d54507c2c6e80dd40de8e8301a26c30d0c148077
FIC <FIC@DESKTOP-15OF2LT>
2021-07-13 16:56:34 -0300

    Api base

---
+++ senai_czbooks_webApi/senai_czbooks_webApi/Domains/TiposUsuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace senai_czbooks_webApi.Domains
+{
+    public partial class TiposUsuario
+    {
+        public TiposUsuario()
+        {
+            Usuarios = new HashSet<Usuario>();
+        }
+
+        public int IdTiposUsuario { get; set; }
+        [Required(ErrorMessage = "É obrigatório definir um nome pro Tipo de Usuário!")]
+        public string TiposUsuario1 { get; set; }
+
+        public virtual ICollection<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ senai_czbooks_webApi/senai_czbooks_webApi/Domains/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace senai_czbooks_webApi.Domains
+{
+    public partial class Usuario
+    {
+        public Usuario()
+        {
+            Autores = new HashSet<Autores>();
+            Clientes = new HashSet<Clientes>();
+        }
+
+        public int IdUsuario { get; set; }
+        public int? IdTiposUsuario { get; set; }
+        [Required(ErrorMessage = "É obrigatório definir uma senha!")]
+        public string Senha { get; set; }
+        [Required(ErrorMessage = "É obrigatório definir um email!")]
+        public string Email { get; set; }
+
+        public virtual TiposUsuario IdTiposUsuarioNavigation { get; set; }
+        public virtual ICollection<Autores> MAutores { get; set; }
+        public virtual ICollection<Clientes> Clientes { get; set; }
+    }
+}
